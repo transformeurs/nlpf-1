e6f0a7f9f7689711fc4a865a1d1e2252e00ab22b
Selma Castella <selma.castella@epita.fr>
2022-09-23 14:08:51 +0200

    [ADD][CANDIDATE] Model for DB

---
+++ backend/src/candidate/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from src.database import Base
+
+class Candidate(Base):
+    __tablename__ = "canditate"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    photo_url = Column(String, index=True)
+    description = Column(String, index=True)
+    pronouns = Column(String, index=True)
