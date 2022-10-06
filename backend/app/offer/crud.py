22f7b4a30672a47adc0e57971bc0daf23891dc5b
Luca Corrieri <luca.corrieri@epita.fr>
2022-10-06 17:39:05 +0200

    Offers API: Improve Offer model and add routes

--- backend/app/offer/crud.py
+++ backend/app/offer/crud.py
@@ -4,26 +4,29 @@ from ..company.models import Company
 
 from . import models, schemas
 
-
 def get_offer(db: Session, offer_id: int):
     return db.query(models.Offer).filter(models.Offer.id == offer_id).first()
 
-def create_offer(db: Session, offer: schemas.OfferCreate):
-    skills = offer.skills.join(",")
-    
+def get_offers(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Offer).offset(skip).limit(limit).all()
+
+def create_offer(db: Session, offer: schemas.OfferCreate, company_id: int):
     db_offer = models.Offer(
         title = offer.title,
         description = offer.description,
-        company_id = offer.company_id,
+        company_id = company_id,
         created_at = offer.created_at,
-        skills = skills
+        skills = offer.skills
     )
     db.add(db_offer)
-    
+
     db.commit()
     db.refresh(db_offer)
     return db_offer
 
 def get_offers_by_company(db: Session, company_id: int):
     company = db.query(Company).filter(Company.id == company_id).first()
+    if not company:
+        return None
+
     return company.offers
--- backend/app/offer/models.py
+++ backend/app/offer/models.py
@@ -1,7 +1,10 @@
-import datetime
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.orm import relationship
+from datetime import datetime
+
 from ..database import Base
+
 
 class Offer(Base):
     __tablename__ = "offer"
@@ -9,8 +12,8 @@ class Offer(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
-    company_id = Column(String, ForeignKey("company.id"), unique=True, index=True)
-    created_at = Column(DateTime(timezone=False), default=datetime.datetime.now())
-    skills = Column(String, index=True)
+    company_id = Column(Integer, ForeignKey("company.id"), index=True)
+    created_at = Column(DateTime(timezone=False), default=datetime.now())
+    skills = Column(ARRAY(String(255)))
 
     company = relationship("Company", back_populates="offers")
